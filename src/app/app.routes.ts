4b53e1135302ed41cfdb695a0839a47ae253fb05
ionichi <feryandi2303@gmail.com>
2024-11-16 17:07:23 +0700

    feat: header, sidebar, footer, content (ecommerce page)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { EcommerceComponent } from './ecommerce/ecommerce.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", redirectTo: "ecommerce", pathMatch: "full" },
+  { path: "ecommerce", component: EcommerceComponent },
+];
